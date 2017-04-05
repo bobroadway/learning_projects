bb01946122de852e3212ea60db63efbc07a9f0f5
bobroadway <broadway.bo@gmail.com>
2017-04-05 21:43:42 +0000

    convert function documentation to docstring standards

--- python/commandline/tutorials/random_number_game.py
+++ python/commandline/tutorials/random_number_game.py
@@ -31,15 +31,13 @@ farewell = ''
 # FUNCTIONS                            #
 ########################################
 
-# function to pring the greeting
-#   returns null
 def print_greeting():
+    """Prints greeting and explanation to the console."""
     print('Lucky Numbers! {} numbers will be generated.'.format(COUNT_GENERATED))
     print('If one of them is a "{}", you lose!'.format(LOSING_NUMBER))
 
-# function to get the list of random numbers
-#   returns numbers: list of randomly generated numbers
 def get_numbers():
+    """Places randomly generated numbers in a list, returns that list."""
     numbers = []
     count = 0
     while count < COUNT_GENERATED:
@@ -47,23 +45,19 @@ def get_numbers():
         numbers.append(number);
         print(number)
         count += 1
-        
     return numbers
 
-# function to retrieve the results of "playing the game"
-#   returns result_message: string indicating if the player won or lost
 def get_result():
+    """Checks if the losing number is found in the list, returns result_message."""
     result_message = ''
     if LOSING_NUMBER in get_numbers():
         result_message = 'Sorry, you lose!'
     else:
         result_message = 'You win!'
-        
     return result_message
     
-# function to determine if the user would like to play again
-#   returns play_again: 'y' or 'n' indicated if the user would like to play again
 def prompt_play_again():
+    """Prompts the user to play again, returns the 'y' or 'n' from the user."""
     play_again = ''
     while play_again not in ('y', 'n'):
         play_again = str(input('Play again? (y/n): ')).lower()
