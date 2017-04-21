7a89a730576caf823d8c82bb07528c64c7eddb84
bobroadway <broadway.bo@gmail.com>
2017-04-21 20:41:25 +0000

    create basic Food class

--- python/commandline/projects/nutritionalValue/food.py
+++ python/commandline/projects/nutritionalValue/food.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3.4
-""" New Class: Food.py """
+""" New Project: Food.py """
 class Food:
-    pass
+    """ Represents a single food item """
+    def __init__(self):
+        print("New Food Object Created")
+        self.name = str(input("What is the name of the food? "))
+        self.size = int(input("Size (in grams)? "))
+        self.fat = int(input("How many grams of fat? "))
+        self.carb = int(input("How many grams of carbs? "))
+        self.protein = int(input("How many grams of protein? "))
+        print("Thank you for the information!")
+        print("------------------------------")
+        
+    def display(self):
+        print(self.name)
+        print("Serving Size: {} grams".format(self.size))
+        print("Fat: {}g".format(self.fat))
+        print("Carbs: {}g".format(self.carb))
+        print("Protein: {}g".format(self.protein))
+        
+## Main ##
+food = Food()
+food.display()
